d4ac42fe0a924cb752ca099d9312cfdcb75147ff
theryshan cooke <theryshanc@gmail.com>
2025-03-01 10:23:22 -0500

    king

--- src/App.tsx
+++ src/App.tsx
@@ -34,8 +34,8 @@ function App() {
           </p>
           <div className="flex justify-center">
             <img 
-              src="https://images.unsplash.com/photo-1497215842964-222b430dc094?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-              // src="/beach.png"
+              // src="https://images.unsplash.com/photo-1497215842964-222b430dc094?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+              src="/cobb.jpg"
               alt="Remote work illustration" 
               className="rounded-lg shadow-lg max-w-full h-auto"
             />
